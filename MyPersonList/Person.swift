77fba8214baf7277a1382006292e2be0cf0826e4
Burenkovuser <nb2207@yandex.ru>
2019-08-09 19:40:00 +0700

    personVC is ready

---
+++ MyPersonList/Person.swift
@@ -0,0 +1,21 @@
+//
+//  Person.swift
+//  MyPersonList
+//
+//  Created by Vasilii on 09/08/2019.
+//  Copyright © 2019 Vasilii Burenkov. All rights reserved.
+//
+
+import Foundation
+
+struct Person {
+    var name: String
+    var surname: String
+    var email: String
+    var phone: String
+}
+
+let names = ["Victor", "Bob", "Ivan", "Vasilii", "Stas"]
+let surnames = ["Kuznecov", "Ivanov", "Petrov", "Sidorov", "Burenkov"]
+let emails = ["aaa@gmail.com", "bbb@gmail.com", "ccc@gmail.com", "ddd@gmail.com", "sss@gmail.com"]
+let phones = ["111-111-11", "222-22-22", "333-33-33", "444-44-44", "555-55-55"]
--- MyPersonList/PersonTableViewController.swift
+++ MyPersonList/PersonTableViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class PersonTableViewController: UITableViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -17,25 +17,35 @@ class PersonTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return names.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let person: Person = createPerson(indexPath: indexPath)
+        cell.textLabel?.text = person.name + " " + person.surname
+        cell.textLabel?.numberOfLines = 0
         return cell
     }
-    */
-
+    
+    // MARK: - privat func
+    
+        func createPerson(indexPath: IndexPath) -> Person {
+        let name: String = names[indexPath.row]
+        let surname: String = surnames[indexPath.row]
+        let email: String = emails[indexPath.row]
+        let phone: String = phones[indexPath.row]
+        let person = Person(name: name, surname: surname, email: email, phone: phone)
+        
+        return person
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
