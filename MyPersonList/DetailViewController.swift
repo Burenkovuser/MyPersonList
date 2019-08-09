aca175a56466c0da8c0d4a340dedb6d23dde70ea
Burenkovuser <nb2207@yandex.ru>
2019-08-09 20:10:14 +0700

    task one is finished

--- MyPersonList/DetailViewController.swift
+++ MyPersonList/DetailViewController.swift
@@ -10,21 +10,19 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    var name: String!
+    var surname: String!
+    var phone: String!
+    var email: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        userNameLabel.text = name + " " + surname
+        phoneLabel.text = "Phone: " + phone
+        emailLabel.text = "Email: " + email
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
